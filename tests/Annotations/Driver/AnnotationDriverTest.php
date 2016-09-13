d2d9f23b701444ece877e737be862c440603d4b8
judicael.paquet <judicael.paquet@batiwiz.com>
2016-09-13 09:23:45 +0200

    Add unit test

---
+++ tests/Annotations/Driver/AnnotationDriverTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace judicaelpaquet\AuthorizationBundle\tests\Annotations\Driver;
+
+use Doctrine\Common\Annotations\Reader;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use judicaelpaquet\AuthorizationBundle\Annotations;
+use Symfony\Component\HttpFoundation\Response;
+
+class AnnotationDriverTest
+{
+    /**
+     * This event will fire during any controller call
+     * @param FilterControllerEvent $event
+     */
+    public function testOnKernelController()
+    {
+
+        $this->assertTrue(false);
+    }
+
+    /**
+     * automatise the unauthorized
+     */
+    public static function showUnauthorized()
+    {
+
+        $array = array('success' => false);
+        $response = new Response(json_encode($array), 401);
+        $response->headers->set('Content-Type', 'application/json');
+        $response->send();
+        exit;
+    }
+}
